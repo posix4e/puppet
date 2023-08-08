e53c3690680d0be3836da9d8b932e6f0a4b65819
vkolgi <vkolgi@gmail.com>
2023-08-08 12:06:20 +0530

    Cleanup E2E tests
    1. For easier local testing, moved SAUCE secrets to .env file
    2. Got rid of unecessary sleeps
    3. Changes required by the past review by GPT bot
    4. Explicit timeout is added in case of waiting for elements to appear.

--- e2e_tests/test_sauce_labs.py
+++ e2e_tests/test_sauce_labs.py
@@ -7,13 +7,23 @@ import os
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.remote.webdriver import WebDriver
+from dotenv import load_dotenv
 
-APPIUM_PORT = 4723
-APPIUM_HOST = "127.0.0.1"
+load_dotenv()
 
+# SAUCE LABS Specific configuration
 SAUCE_USERNAME = os.getenv("SAUCE_USERNAME")
 SAUCE_ACCESS_KEY = os.getenv("SAUCE_ACCESS_KEY")
 TEST_UUID = os.getenv("TEST_UUID", "")
+BUILD_STAGE = os.getenv("BUILD_STAGE", "DEV")
+
+# Sauce labs URL
+APPIUM_SERVER_URL = "http://127.0.0.1:4723"
+SAUCE_LABS_URL = "https://ondemand.us-west-1.saucelabs.com:443/wd/hub"
+
+# Android specific application
+APP_PACKAGE_NAME = "com.ttt246.puppet"
+APP_ACTIVITY_NAME = ".ChatterAct"
 
 
 def create_android_driver(sauce_labs=False):
@@ -21,60 +31,77 @@ def create_android_driver(sauce_labs=False):
         platformName="Android",
         automationName="uiautomator2",
         deviceName="Android",
-        appPackage="com.ttt246.puppet",
-        appActivity=".ChatterAct",
+        appPackage=APP_PACKAGE_NAME,
+        appActivity=APP_ACTIVITY_NAME,
         language="en",
         locale="US",
     )
-    capabilities["appium:app"] = "app-release-unsigned.apk"
-    capabilities["autoGrantPermissions"] = "true"
-    connection_url = f"http://{APPIUM_HOST}:{APPIUM_PORT}"
+
+    capabilities["appium:autoGrantPermissions"] = "true"
+    capabilities["appium:autoAcceptAlerts"] = "true"
+    capabilities["appium:automationName"] = "uiautomator2"
+
     if sauce_labs:
-        capabilities["platformName"] = "Android"
-        capabilities["appium:app"] = "storage:filename=app-release-unsigned.apk"
-        capabilities["appium:deviceName"] = "Android GoogleAPI Emulator"
         capabilities["appium:deviceOrientation"] = "portrait"
         capabilities["appium:platformVersion"] = "13.0"
-        capabilities["appium:automationName"] = "uiautomator2"
-        capabilities["appPackage"] = "com.ttt246.puppet"
-        capabilities["appActivity"] = ".ChatterAct"
-        capabilities["appium:autoGrantPermissions"] = "true"
         capabilities["sauce:options"] = {}
         capabilities["sauce:options"]["username"] = SAUCE_USERNAME
         capabilities["sauce:options"]["accessKey"] = SAUCE_ACCESS_KEY
-        capabilities["sauce:options"]["build"] = "appium-build-HGNZD"
-        capabilities["sauce:options"]["name"] = "Automate Tests"
+        capabilities["sauce:options"]["build"] = f"puppet-build-{BUILD_STAGE}"
+        capabilities["sauce:options"]["name"] = "Test Android"
         capabilities["appium:app"] = "storage:filename=app-release-unsigned.apk"
         capabilities["appium:deviceName"] = "Android GoogleAPI Emulator"
 
         connection_url = "https://ondemand.us-west-1.saucelabs.com:443/wd/hub"
+    else:
+        connection_url = APPIUM_SERVER_URL
+
 
     return webdriver.Remote(connection_url, capabilities)
 
 
 @contextmanager
 def android_driver(options):
-    # prefer this fixture if there is no need to customize driver options in tests
     driver = create_android_driver(sauce_labs=True)
+    driver.implicitly_wait(10)
     yield driver
     driver.quit()
 
 
 def save_server_settings(driver: WebDriver):
-    el8 = driver.find_element(
-        by=AppiumBy.ID, value="com.ttt246.puppet:id/settingsButton"
+    element = driver.find_element(
+        by=AppiumBy.ID, value=f"{APP_PACKAGE_NAME}:id/settingsButton"
     )
-    el8.click()
-    el9 = driver.find_element(
-        by=AppiumBy.ID, value="com.ttt246.puppet:id/serverUrlEditText"
+    element.click()
+    element = driver.find_element(
+        by=AppiumBy.ID, value=f"{APP_PACKAGE_NAME}:id/serverUrlEditText"
     )
-    el9.send_keys("https://posix4e-puppet.hf.space")
-    el10 = driver.find_element(
-        by=AppiumBy.ID, value="com.ttt246.puppet:id/uuidEditText"
+    element.send_keys("https://posix4e-puppet.hf.space")
+    element = driver.find_element(
+        by=AppiumBy.ID, value=f"{APP_PACKAGE_NAME}:id/uuidEditText"
     )
-    el10.send_keys("1608052e-b294-4a6f-a69f-e18c9bedf5c8")
-    el11 = driver.find_element(by=AppiumBy.ID, value="com.ttt246.puppet:id/saveButton")
-    el11.click()
+    element.send_keys(TEST_UUID)
+    element = driver.find_element(
+        by=AppiumBy.ID, value="com.ttt246.puppet:id/saveButton"
+    )
+    element.click()
+
+
+def wait_for_element(
+    driver: WebDriver, by_selector: str, selector_value: str, timeout: int = 2
+):
+    from selenium.webdriver.support.wait import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+
+    try:
+        element = WebDriverWait(driver, timeout).until(
+            EC.presence_of_element_located((by_selector, selector_value))
+        )
+
+    except:
+        element = None
+    finally:
+        return element
 
 
 def enable_privacy_settings(driver: WebDriver):
@@ -86,10 +113,9 @@ def enable_privacy_settings(driver: WebDriver):
     # Use Puppet button
     if not primary_checkbox.get_attribute("checked") == "true":
         primary_checkbox.click()
-        try:
-            driver.find_element(by=AppiumBy.XPATH, value="//*[@text='Allow']").click()
-        except Exception as e:
-            pass
+        element = wait_for_element(driver, AppiumBy.XPATH, "//*[@text='Allow']")
+        if element:
+            element.click()
 
     # Puppet shortcut button
     secondary_checkbox = driver.find_element(
@@ -97,47 +123,43 @@ def enable_privacy_settings(driver: WebDriver):
     )
     if not secondary_checkbox.get_attribute("checked") == "true":
         secondary_checkbox.click()
-        try:
-            driver.find_element(by=AppiumBy.XPATH, value="//*[@text='Got it']").click()
-        except Exception as e:
-            pass
 
-    time.sleep(2)
+    # driver.start_activity(APP_PACKAGE_NAME, APP_ACTIVITY_NAME)
+    # driver.activate_app("com.ttt246.puppet")
+    # These sleeps are harsh, however are now needed till will fix the activity which brings up settings by default
+    # Once thats fixed, we can activate the app back it should resume as is, without the back button
+
     driver.back()
-    time.sleep(2)
+    time.sleep(1)
     driver.back()
+    time.sleep(1)
 
 
 class TestAppium(unittest.TestCase):
     def test_tab_4(self):
-        import time
-
         # Usage of the context manager ensures the driver session is closed properly
         # after the test completes. Otherwise, make sure to call `driver.quit()` on teardown.
         with android_driver({}) as driver:
-            driver.implicitly_wait(10)
-
             enable_privacy_settings(driver)
 
-            driver.activate_app("com.ttt246.puppet")
-            driver.wait_activity(".ChatterAct", 5)
-            time.sleep(2)
+            driver.wait_activity(APP_ACTIVITY_NAME, 5)
 
             save_server_settings(driver)
 
-            el14 = driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Tab 4"]')
-            el14.click()
-            el15 = driver.find_element(
+            element = driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Tab 4"]')
+            element.click()
+            element = driver.find_element(
                 by=AppiumBy.XPATH, value="//android.widget.EditText[@hint='UID']"
             )
-            el15.send_keys(TEST_UUID)
-            el16 = driver.find_element(
+            element.send_keys(TEST_UUID)
+            element = driver.find_element(
                 by=AppiumBy.XPATH, value="//android.widget.EditText[@hint='Command']"
             )
-            el16.send_keys("test")
-            el17 = driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Submit"]')
-            el17.click()
-            time.sleep(5)
+            element.send_keys("test")
+            element = driver.find_element(
+                by=AppiumBy.XPATH, value='//*[@text="Submit"]'
+            )
+            element.click()
 
 
 if __name__ == "__main__":
