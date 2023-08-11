f6bab1b0d5a18994f88166d815c21286538b6ea6
vkolgi <vkolgi@gmail.com>
2023-08-11 23:31:23 +0530

    Run tests in real android device
    Changed test to run in real android device in saucelabs.
    Also made changes according to the bot review.

--- e2e_tests/utils.py
+++ e2e_tests/utils.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.common.exceptions import TimeoutException
 
 
 def wait_for_element(
@@ -7,12 +8,14 @@ def wait_for_element(
     from selenium.webdriver.support.wait import WebDriverWait
     from selenium.webdriver.support import expected_conditions as EC
 
+    element = None
+
     try:
         element = WebDriverWait(driver, timeout).until(
             EC.presence_of_element_located((by_selector, selector_value))
         )
 
-    except:
+    except TimeoutException:
         element = None
     finally:
         return element
@@ -21,4 +24,3 @@ def wait_for_element(
 def find_and_click(driver: WebDriver, by_selector: str, selector_value: str):
     element = driver.find_element(by=by_selector, value=selector_value)
     element.click()
-    return element
